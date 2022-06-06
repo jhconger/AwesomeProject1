2b60e2423d3204ae9dbad32d58f35dba217b4468
jhconger <jhconger@gmail.com>
2022-06-05 19:50:53 -0500

    Initial Commit

---
+++ Shared/AwesomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeProjectApp.swift
+//  Shared
+//
+//  Created by justin conger on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
